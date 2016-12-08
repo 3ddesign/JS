3c5c982cfce6cc74774146de54d1dd6f2e5f3469
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-08 08:32:04 +0200

    hw2 0.1

--- Homework 01-2/Task1/js/script.js
+++ Homework 01-2/Task1/js/script.js
@@ -1,21 +1,20 @@
-
 function pow(a, b) {
-var result = 1;
+    var result = 1;
 
-for (var i = 0; i < b; i++) {
-  result *= a;
+    for (var i = 0; i < b; i++) {
+        result *= a;
+    }
+    console.log(a + ' в степени ' + b + ' равно ' + result);
 }
-  console.log(a + ' в степени ' + b + ' равно ' + result);
- }
 
 var num = prompt("Введите целое число:");
-if ( parseInt(num) != num ) {
+if (parseInt(num) != num) {
     alert("Вы ввели не целое число!");
 } else {
-var degree = prompt("Введите степень:");
-if (degree < 0) {
-  alert("Введите корректное значение стемени")
-} else {
-  pow(num, degree)
-}
+    var degree = prompt("Введите степень:");
+    if (degree < 0) {
+        alert("Введите корректное значение стемени")
+    } else {
+        pow(num, degree)
+    }
 }
--- Homework 01-2/Task2/js/script.js
+++ Homework 01-2/Task2/js/script.js
@@ -3,7 +3,7 @@
 var arr = [];
 
 for (var i = 0; i < 5; i++) {
-arr.push(prompt('Введите имя ' + i + ':'));
+    arr.push(prompt('Введите имя ' + i + ':'));
 }
 
 var name = prompt('Введите имя пользователя:');
@@ -11,17 +11,14 @@ var flag;
 
 //Осуществляем поиск по массиву
 for (var i = 0; i < 5; i++) {
-if (name == arr[i])
-{
- var flag = true;
-}
-else {}
+    if (name == arr[i]) {
+        var flag = true;
+    } else {}
 }
 
 //Проверяем значение флага и выводим сообще
-if(flag) {
-  alert(name + ", вы успешно вошли.");
-}
-else {
-  alert("Ошибка! Такого имени нет в массиве");
+if (flag) {
+    alert(name + ", вы успешно вошли.");
+} else {
+    alert("Ошибка! Такого имени нет в массиве");
 }
