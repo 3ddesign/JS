f7f4c4ea4fd7d7db461ff57a0b00b106fff41f02
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-09 17:41:53 +0200

    hw3 0.2

--- Homework 05-6/Task1/js/script.js
+++ Homework 05-6/Task1/js/script.js
@@ -1,15 +1,52 @@
-window.onload = function () {
+window.onload = function() {
 
-var start = document.getElementById('startButton')
-var clear = document.getElementById('clearButton')
-var display = document.getElementsByName('h4')
+    var startB = document.getElementById('startButton');
+    var clearB = document.getElementById('clearButton');
+    var display = document.getElementById('displayTime');
+    var displaySec = document.getElementById('displaySeconds');
+    var counter = 0;
+    var counterMain = 0;
+    var clearPress = true;
 
-console.log(start);
 
-function startHandler(){
-  alert("work!");
-}
+    function someFunction() {
+        displaySec.innerHTML = counter;
+        counter++;
+        if (counter > 100) {
+            counter = 0;
+            counterMain++;
+            displayTime.innerHTML = counterMain;
+         }
 
-start.addEventListener('click', startHandler)
+    }
+
+    function startHandler() {
+        timer = setInterval(someFunction, 10);
+    }
+
+    startB.addEventListener('click', startHandler);
+    clearB.addEventListener('click', clearHandler);
+
+
+
+
+    function clearHandler() {
+        if (clearPress == true) {
+            clearTimeout(timer);
+            clearB.className = 'btn btn-danger';
+            clearB.innerHTML = 'Clear';
+            clearPress = false;
+        } else {
+            clearInterval(timer);
+            counter = 0;
+            display.innerHTML = counter;
+            clearB.className = 'btn btn-warning';
+            clearB.innerHTML = 'Pause';
+            clearPress = true;
+        }
+    }
+
+
+
 
 }
