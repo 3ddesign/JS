8ce05931f26711be2ea31061e8f439e8bbf44324
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-10 12:10:03 +0200

    hw3 1.0

---
+++ Homework 05-6/Task2/js/script.js
@@ -0,0 +1,111 @@
+window.onload = function() {
+
+    //find all elements and init variables:
+    var startB = document.getElementById('startButton');
+    var splitB = document.getElementById('splitButton');
+    var resetB = document.getElementById('resetButton');
+    var displaySec1 = document.getElementById('displaySec1');
+    var displaySec2 = document.getElementById('displaySec2');
+    var displayMin1 = document.getElementById('displayMin1');
+    var displayMin2 = document.getElementById('displayMin2');
+    var displayHou1 = document.getElementById('displayHou1');
+    var displayHou2 = document.getElementById('displayHou2');
+    var displaySecFull = document.getElementById('displaySeconds');
+    var counter = 0;
+    var counterSec1 = 0;
+    var counterSec2 = 0;
+    var counterMin1 = 0;
+    var counterMin2 = 0;
+    var counterHou1 = 0;
+    var counterHou2 = 0;
+    var clearPress = true;
+
+    //Big display function (to 24Hours):
+    function someFunction() {
+        displaySecFull.innerHTML = counter;
+        counter++;
+        if (counter > 100) {
+            counter = 0;
+            counterSec1++;
+            displaySec1.innerHTML = counterSec1;
+            if (counterSec1 > 9) {
+                displaySec1.innerHTML = 0;
+                counterSec1 = 0;
+                counterSec2++;
+                displaySec2.innerHTML = counterSec2;
+                if (counterSec2 > 5) {
+                    displaySec2.innerHTML = 0;
+                    counterSec2 = 0;
+                    counterMin1++;
+                    displayMin1.innerHTML = counterMin1;
+                    if (counterMin1 > 9) {
+                        displayMin1.innerHTML = 0;
+                        counterMin1 = 0;
+                        counterMin2++;
+                        displayMin2.innerHTML = counterMin2;
+                        if (counterMin2 > 5) {
+                            displayMin2.innerHTML = 0;
+                            counterMin2 = 0;
+                            counterHou1++;
+                            displayHou1.innerHTML = counterHou1;
+                            if (counterHou1 > 9) {
+                                displayHou1.innerHTML = 0;
+                                counterHou1 = 0;
+                                counterHou1++;
+                                displayHou2.innerHTML = counterHou2;
+                                if (counterHou2 > 23) {
+                                    clearInterval(timer);
+                                    startHandler();
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    //Clear function:
+    function clearHandler() {
+        clearInterval(timer);
+        counter = 0;
+        counterSec1 = 0;
+        counterSec2 = 0;
+        counterMin1 = 0;
+        counterMin2 = 0;
+        counterHou1 = 0;
+        counterHou2 = 0;
+        displaySec1.innerHTML = counterSec1;
+        displaySec2.innerHTML = counterSec2;
+        displayMin1.innerHTML = counterMin1;
+        displayMin2.innerHTML = counterMin2;
+        displayHou1.innerHTML = counterHou1;
+        displayHou2.innerHTML = counterHou2;
+        displaySecFull.innerHTML = counter;
+        clearPress = true
+        startB.className = 'btn btn-success';
+        startB.innerHTML = 'Start';
+    }
+
+    //Handlers:
+    function startHandler() {
+        if (clearPress == true) {
+            timer = setInterval(someFunction, 10);
+            startB.className = 'btn btn-warning';
+            startB.innerHTML = 'Pause';
+            clearPress = false
+        } else {
+            clearTimeout(timer);
+            clearPress = true
+            startB.className = 'btn btn-success';
+            startB.innerHTML = 'Start';
+        }
+    }
+
+    //Listeners:
+    startB.addEventListener('click', startHandler);
+    resetB.addEventListener('click', clearHandler);
+}
