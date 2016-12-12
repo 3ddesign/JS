428fe58f6a255e1fb21b22e163fc1d217764333d
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-12 16:24:05 +0200

    hw3 1.2

--- Homework 05-6/Task1/js/script.js
+++ Homework 05-6/Task1/js/script.js
@@ -10,7 +10,11 @@ window.onload = function() {
     var displayHou1 = document.getElementById('displayHou1');
     var displayHou2 = document.getElementById('displayHou2');
     var displaySecFull = document.getElementById('displaySeconds');
+    var displaySecFull1 = document.getElementById('displaySeconds1');
+    var displaySecFull2 = document.getElementById('displaySeconds2');
     var counter = 0;
+    var counterMil1 = 0;
+    var counterMil2 = 0;
     var counterSec1 = 0;
     var counterSec2 = 0;
     var counterMin1 = 0;
@@ -23,60 +27,76 @@ window.onload = function() {
     function someFunction() {
         displaySecFull.innerHTML = counter;
         counter++;
-        if (counter > 1000) {
+        if (counter > 9) {
             counter = 0;
-            counterSec1++;
-            displaySec1.innerHTML = counterSec1;
-            if (counterSec1 > 9) {
-                displaySec1.innerHTML = 0;
-                counterSec1 = 0;
-                counterSec2++;
-                displaySec2.innerHTML = counterSec2;
-                if (counterSec2 > 5) {
-                    displaySec2.innerHTML = 0;
-                    counterSec2 = 0;
-                    counterMin1++;
-                    displayMin1.innerHTML = counterMin1;
-                    if (counterMin1 > 9) {
-                        displayMin1.innerHTML = 0;
-                        counterMin1 = 0;
-                        counterMin2++;
-                        displayMin2.innerHTML = counterMin2;
-                        if (counterMin2 > 5) {
-                            displayMin2.innerHTML = 0;
-                            counterMin2 = 0;
-                            counterHou1++;
-                            displayHou1.innerHTML = counterHou1;
-                            if (counterHou1 > 9) {
-                                displayHou1.innerHTML = 0;
-                                counterHou1 = 0;
-                                counterHou1++;
-                                displayHou2.innerHTML = counterHou2;
-                                if (counterHou2 > 23) {
-                                    clearInterval(timer);
-                                    startHandler();
+            counterMil1++;
+            displaySecFull1.innerHTML = counterMil1;
+            if (counterMil1 > 9) {
+              displaySecFull1.innerHTML = 0;
+                counterMil1 = 0;
+                counterMil2++;
+                displaySecFull2.innerHTML = counterMil2;
+                if (counterMil2 > 9) {
+                  displaySecFull2.innerHTML = 0;
+                    counterMil2 = 0;
+                    counterSec1++;
+                    displaySec1.innerHTML = counterSec1;
+                    if (counterSec1 > 9) {
+                        displaySec1.innerHTML = 0;
+                        counterSec1 = 0;
+                        counterSec2++;
+                        displaySec2.innerHTML = counterSec2;
+                        if (counterSec2 > 5) {
+                            displaySec2.innerHTML = 0;
+                            counterSec2 = 0;
+                            counterMin1++;
+                            displayMin1.innerHTML = counterMin1;
+                            if (counterMin1 > 9) {
+                                displayMin1.innerHTML = 0;
+                                counterMin1 = 0;
+                                counterMin2++;
+                                displayMin2.innerHTML = counterMin2;
+                                if (counterMin2 > 5) {
+                                    displayMin2.innerHTML = 0;
+                                    counterMin2 = 0;
+                                    counterHou1++;
+                                    displayHou1.innerHTML = counterHou1;
+                                    if (counterHou1 > 9) {
+                                        displayHou1.innerHTML = 0;
+                                        counterHou1 = 0;
+                                        counterHou1++;
+                                        displayHou2.innerHTML = counterHou2;
+                                        if (counterHou2 > 23) {
+                                            clearInterval(timer);
+                                            startHandler();
+                                        }
+                                    }
                                 }
+
                             }
                         }
-
                     }
+
                 }
+
             }
-
         }
-
     }
 
     //Clear function:
     function clearHandler() {
         clearInterval(timer);
         counter = 0;
+        counterMil1 = 0;
+        counterMil2 = 0;
         counterSec1 = 0;
         counterSec2 = 0;
         counterMin1 = 0;
         counterMin2 = 0;
         counterHou1 = 0;
         counterHou2 = 0;
+        displaySecFull1.innerHTML = counterMil1;
+        displaySecFull2.innerHTML = counterMil2;
         displaySec1.innerHTML = counterSec1;
         displaySec2.innerHTML = counterSec2;
         displayMin1.innerHTML = counterMin1;
--- Homework 05-6/Task2/js/script.js
+++ Homework 05-6/Task2/js/script.js
@@ -12,7 +12,11 @@ window.onload = function() {
     var displayHou1 = document.getElementById('displayHou1');
     var displayHou2 = document.getElementById('displayHou2');
     var displaySecFull = document.getElementById('displaySeconds');
+    var displaySecFull1 = document.getElementById('displaySeconds1');
+    var displaySecFull2 = document.getElementById('displaySeconds2');
     var counter = 0;
+    var counterMil1 = 0;
+    var counterMil2 = 0;
     var counterSec1 = 0;
     var counterSec2 = 0;
     var counterMin1 = 0;
@@ -32,48 +36,60 @@ window.onload = function() {
     function someFunction() {
         displaySecFull.innerHTML = counter;
         counter++;
-        if (counter > 100) {
+        if (counter > 9) {
             counter = 0;
-            counterSec1++;
-            displaySec1.innerHTML = counterSec1;
-            if (counterSec1 > 9) {
-                displaySec1.innerHTML = 0;
-                counterSec1 = 0;
-                counterSec2++;
-                displaySec2.innerHTML = counterSec2;
-                if (counterSec2 > 5) {
-                    displaySec2.innerHTML = 0;
-                    counterSec2 = 0;
-                    counterMin1++;
-                    displayMin1.innerHTML = counterMin1;
-                    if (counterMin1 > 9) {
-                        displayMin1.innerHTML = 0;
-                        counterMin1 = 0;
-                        counterMin2++;
-                        displayMin2.innerHTML = counterMin2;
-                        if (counterMin2 > 5) {
-                            displayMin2.innerHTML = 0;
-                            counterMin2 = 0;
-                            counterHou1++;
-                            displayHou1.innerHTML = counterHou1;
-                            if (counterHou1 > 9) {
-                                displayHou1.innerHTML = 0;
-                                counterHou1 = 0;
-                                counterHou1++;
-                                displayHou2.innerHTML = counterHou2;
-                                if (counterHou2 > 23) {
-                                    clearInterval(timer);
-                                    startHandler();
+            counterMil1++;
+            displaySecFull1.innerHTML = counterMil1;
+            if (counterMil1 > 9) {
+              displaySecFull1.innerHTML = 0;
+                counterMil1 = 0;
+                counterMil2++;
+                displaySecFull2.innerHTML = counterMil2;
+                if (counterMil2 > 9) {
+                  displaySecFull2.innerHTML = 0;
+                    counterMil2 = 0;
+                    counterSec1++;
+                    displaySec1.innerHTML = counterSec1;
+                    if (counterSec1 > 9) {
+                        displaySec1.innerHTML = 0;
+                        counterSec1 = 0;
+                        counterSec2++;
+                        displaySec2.innerHTML = counterSec2;
+                        if (counterSec2 > 5) {
+                            displaySec2.innerHTML = 0;
+                            counterSec2 = 0;
+                            counterMin1++;
+                            displayMin1.innerHTML = counterMin1;
+                            if (counterMin1 > 9) {
+                                displayMin1.innerHTML = 0;
+                                counterMin1 = 0;
+                                counterMin2++;
+                                displayMin2.innerHTML = counterMin2;
+                                if (counterMin2 > 5) {
+                                    displayMin2.innerHTML = 0;
+                                    counterMin2 = 0;
+                                    counterHou1++;
+                                    displayHou1.innerHTML = counterHou1;
+                                    if (counterHou1 > 9) {
+                                        displayHou1.innerHTML = 0;
+                                        counterHou1 = 0;
+                                        counterHou1++;
+                                        displayHou2.innerHTML = counterHou2;
+                                        if (counterHou2 > 23) {
+                                            clearInterval(timer);
+                                            startHandler();
+                                        }
+                                    }
                                 }
+
                             }
                         }
-
                     }
+
                 }
+
             }
-
         }
-
     }
 
     //Clear(Reset) function:
@@ -81,12 +97,16 @@ window.onload = function() {
         clearInterval(timer);
         splitcouner = 0;
         counter = 0;
+        counterMil1 = 0;
+        counterMil2 = 0;
         counterSec1 = 0;
         counterSec2 = 0;
         counterMin1 = 0;
         counterMin2 = 0;
         counterHou1 = 0;
         counterHou2 = 0;
+        displaySecFull1.innerHTML = counterMil1;
+        displaySecFull2.innerHTML = counterMil2;
         displaySec1.innerHTML = counterSec1;
         displaySec2.innerHTML = counterSec2;
         displayMin1.innerHTML = counterMin1;
@@ -103,7 +123,7 @@ window.onload = function() {
     // Other Handlers:
     function startHandler() {
         if (clearPress === true) {
-            timer = setInterval(someFunction, 10);
+            timer = setInterval(someFunction, 1);
             startB.className = 'btn btn-warning';
             startB.innerHTML = 'Stop';
             clearPress = false;
@@ -121,7 +141,7 @@ window.onload = function() {
     function splitHandler() {
         splitcouner++;
         var newP = document.createElement('p');
-        newP.innerHTML = '<p>' + splitcouner + splitdisp + counterHou2 + counterHou1 + ':' + counterMin2 + counterMin1 + ':' + counterSec2 + counterSec1 + '.' + counter + '</p>';
+        newP.innerHTML = '<p>' + splitcouner + splitdisp + counterHou2 + counterHou1 + ':' + counterMin2 + counterMin1 + ':' + counterSec2 + counterSec1 + '.' + counterMil2 + counterMil1 + counter + '</p>';
         splitDisp.appendChild(newP);
     }
 };
