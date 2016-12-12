68c6ae37c5659950cf66808ded1c8ea8cf2178d8
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-12 21:57:43 +0200

    hw3 1.4

--- Homework 05-6/Task1/js/script.js
+++ Homework 05-6/Task1/js/script.js
@@ -3,15 +3,8 @@ window.onload = function() {
     //find all elements and init variables:
     var startB = document.getElementById('startButton');
     var clearB = document.getElementById('clearButton');
-    var displaySec1 = document.getElementById('displaySec1');
-    var displaySec2 = document.getElementById('displaySec2');
-    var displayMin1 = document.getElementById('displayMin1');
-    var displayMin2 = document.getElementById('displayMin2');
-    var displayHou1 = document.getElementById('displayHou1');
-    var displayHou2 = document.getElementById('displayHou2');
     var displaySecFull = document.getElementById('displaySeconds');
-    var displaySecFull1 = document.getElementById('displaySeconds1');
-    var displaySecFull2 = document.getElementById('displaySeconds2');
+    var displayAll = document.getElementById('display');
     var counter = 0;
     var counterMil1 = 0;
     var counterMil2 = 0;
@@ -23,67 +16,52 @@ window.onload = function() {
     var counterHou2 = 0;
     var clearPress = true;
 
-    //Big display function (to 24Hours):
-    function someFunction() {
-        displaySecFull.innerHTML = counter;
+    // Display function (to 24Hours):
+    function CountFunction() {
+
         counter++;
-        if (counter > 9) {
+        displayAll.innerHTML = '' + counterHou2 + counterHou1 + ':' + counterMin2 + counterMin1 + ':' + counterSec2 + counterSec1;
+        displaySecFull.innerHTML = '' + counterMil2 + counterMil1 + counter;
+
+        if (counter >= 9) {
             counter = 0;
             counterMil1++;
-            displaySecFull1.innerHTML = counterMil1;
             if (counterMil1 > 9) {
-              displaySecFull1.innerHTML = 0;
                 counterMil1 = 0;
                 counterMil2++;
-                displaySecFull2.innerHTML = counterMil2;
                 if (counterMil2 > 9) {
-                  displaySecFull2.innerHTML = 0;
                     counterMil2 = 0;
                     counterSec1++;
-                    displaySec1.innerHTML = counterSec1;
                     if (counterSec1 > 9) {
-                        displaySec1.innerHTML = 0;
                         counterSec1 = 0;
                         counterSec2++;
-                        displaySec2.innerHTML = counterSec2;
                         if (counterSec2 > 5) {
-                            displaySec2.innerHTML = 0;
                             counterSec2 = 0;
                             counterMin1++;
-                            displayMin1.innerHTML = counterMin1;
                             if (counterMin1 > 9) {
-                                displayMin1.innerHTML = 0;
                                 counterMin1 = 0;
                                 counterMin2++;
-                                displayMin2.innerHTML = counterMin2;
                                 if (counterMin2 > 5) {
-                                    displayMin2.innerHTML = 0;
                                     counterMin2 = 0;
                                     counterHou1++;
-                                    displayHou1.innerHTML = counterHou1;
                                     if (counterHou1 > 9) {
-                                        displayHou1.innerHTML = 0;
                                         counterHou1 = 0;
                                         counterHou1++;
-                                        displayHou2.innerHTML = counterHou2;
                                         if (counterHou2 > 23) {
                                             clearInterval(timer);
                                             startHandler();
                                         }
                                     }
                                 }
-
                             }
                         }
                     }
-
                 }
-
             }
         }
     }
 
-    //Clear function:
+    //Handlers:
     function clearHandler() {
         clearInterval(timer);
         counter = 0;
@@ -95,24 +73,16 @@ window.onload = function() {
         counterMin2 = 0;
         counterHou1 = 0;
         counterHou2 = 0;
-        displaySecFull1.innerHTML = counterMil1;
-        displaySecFull2.innerHTML = counterMil2;
-        displaySec1.innerHTML = counterSec1;
-        displaySec2.innerHTML = counterSec2;
-        displayMin1.innerHTML = counterMin1;
-        displayMin2.innerHTML = counterMin2;
-        displayHou1.innerHTML = counterHou1;
-        displayHou2.innerHTML = counterHou2;
-        displaySecFull.innerHTML = counter;
         clearPress = true;
+        displayAll.innerHTML = '' + counterHou2 + counterHou1 + ':' + counterMin2 + counterMin1 + ':' + counterSec2 + counterSec1;
+        displaySecFull.innerHTML = '' + counterMil2 + counterMil1 + counter;
         startB.className = 'btn btn-success';
         startB.innerHTML = 'Start';
     }
 
-    //Handler:
     function startHandler() {
         if (clearPress === true) {
-            timer = setInterval(someFunction, 1);
+            timer = setInterval(CountFunction, 1);
             startB.className = 'btn btn-warning';
             startB.innerHTML = 'Pause';
             clearPress = false;
