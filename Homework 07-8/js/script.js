6597eebb021c84d165f1efb2c73c8a5377e2beb0
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-13 16:07:00 +0200

    hw4 0.3

--- Homework 07-8/js/script.js
+++ Homework 07-8/js/script.js
@@ -1,4 +1,27 @@
-$(document).ready(function () {
-console.log('works!');
+$(document).ready(function() {
 
+$('div.tab2').hide();
+$('div.tab3').hide();
+
+  $('li.tab1').click(function(){
+  alert('test');
+  $('div.tab2').show();
+    });
 });
+
+
+
+// $(document).ready(function() {
+//
+// $('ul.tabs li').css('cursor', 'pointer');
+//
+// $('ul.tabs.tabs1 li').click(function(){
+//   var thisClass = this.className.slice(0,2);
+//   $('div.t1').hide();
+//   $('div.t2').hide();
+//   $('div.' + thisClass).show();
+//   $('ul.tabs.tabs1 li').removeClass('tab-current');
+//   $(this).addClass('tab-current');
+//   });
+//
+// });
