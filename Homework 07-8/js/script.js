f04b7d33520e1169828123fa73a4f8bc6d5dd302
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-15 12:58:17 +0200

    hw4 1.0

--- Homework 07-8/js/script.js
+++ Homework 07-8/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
+    //Tabs:
     $('div.window2').hide();
     $('div.window3').hide();
-
     $('li').click(function() {
         $('div.window1').hide();
         $('div.window2').hide();
@@ -10,10 +10,44 @@ $(document).ready(function() {
         $('div.window' + tab).show();
     });
 
-      $('#firstname').hover(function() {
 
-$('#firstname').append('<div id="third">Третий</div>').css('background', 'red');
+    //Tooltips:
+    $('#firstname').hover(function() {
+            $('.t1').remove();
+            $('#tools').parent().append('<div class="t1"><h5>Please provide your firstname.</h5></div>');
+            $('.t1').fadeIn(500);
 
-      });
+        },
+        function() {
+            $('.t1').fadeOut(300);
+        });
 
+    $('#lastname').hover(function() {
+            $('.t1').remove();
+            $('.t2').remove();
+            $('#tools').parent().append('<div class="t2"><h5>Please provide also your lastname.</h5></div>');
+            $('.t2').fadeIn(500);
+        },
+        function() {
+            $('.t2').fadeOut(300);
+        });
+
+    $('#address').hover(function() {
+            $('.t3').remove();
+
+            $('.t1').remove();
+
+            $('#tools').parent().append('<div class="t3"><h5>Your home or work address.</h5></div>');
+            $('.t3').fadeIn(500);
+        },
+        function() {
+            $('.t3').fadeOut(300);
+        });
+
+    $('#help').click(function() {
+        $('#tools').parent().append('<div class="t1"><h5>Please provide your firstname.</h5></div>');
+        $('.t1').fadeIn(500);
+        $('.t2').fadeIn(500);
+        $('.t3').fadeIn(500);
+    });
 });
