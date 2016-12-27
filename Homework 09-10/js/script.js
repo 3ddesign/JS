50d422956a59e42a6ed484a0c81af399d38af591
Dmitry Scherbakov <3ddesign@bk.ru>
2016-12-27 17:12:03 +0200

    hw5 0.6

--- Homework 09-10/js/script.js
+++ Homework 09-10/js/script.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
 
-
     $( '.dropdown' ).hover(
         function(){
             $(this).children('.sub-menu').slideDown(300);
+            $(this).children('.sub-menu').hover(function() {
+
+                      $(this).css('background', '#045F94');
+                  },
+                  function() {
+                        $(this).css('background', '#258dc8');
+
+ });
+
         },
         function(){
-            $(this).children('.sub-menu').slideUp(300);
+            $(this).children('.sub-menu').delay(200).slideUp(300);
         }
     );
-
 
     $("#default-usage-select").selectbox().bind('change', function() {
         $('<div>Value of #default-usage-select changed to: ' + $(this).val() + '</div>').appendTo('#demo-default-usage .demoTarget').fadeOut(5000, function() {
