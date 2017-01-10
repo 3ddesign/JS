4273a2e5d47e8aa5901d248609f4fbc1c5b8f91d
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-10 15:44:12 +0200

    hw6 0.1

---
+++ Homework 11-12/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var leftUIEl = $('.carousel-arrow-left');
+    var rightUIEl = $('.carousel-arrow-right');
+
+    leftUIEl.click(function() {});
+
+    rightUIEl.click(function() {});
+
+});
