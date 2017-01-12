5ad5c482885c1ed05519939580d6c7f023f83ec9
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-12 13:45:20 +0200

    hw6 0.4

--- Homework 11-12/Task2/js/script.js
+++ Homework 11-12/Task2/js/script.js
@@ -3,31 +3,27 @@ $(function() {
     var html = $('#mycont').html();
 
 
-    var contentArr = [{
-            h1: 'Хочу учить форнтенд, потому что:',
-            text: 'Хочу видеть результат работы сразу',
-            hr: '<hr>',
-        },
-        {
-            h1: 'Мой контактный телефон:',
-            text: '+380673926228',
-            hr: '<hr>',
-        },
-        {
-            h1: 'Мой профиль в Фейсбук:',
-            text: 'facebook.com/dmitry.scherbakov.585',
-            hr: '<hr>',
-        },
-        {
-            h1: 'Мой фидбек:',
-            text: 'Если нужно, могу сверстать такую страничку)',
-        }   ];
+    var myInfo = {
+        name: 'ЩЕРБАКОВ ДМИТРИЙ ИВАНОВИЧ',
+        img_path: 'img/myphoto.jpg',
+        education: 'Студент ВМУРоЛ "Украина"',
+        header1: 'Хочу учить форнтенд, потому что:',
+        ul1: 'Хочу видеть результат работы сразу',
+        ul2: 'Люблю изучать новые технологии',
+        ul3: 'Надоела рутинная работа',
+        header2: 'Мой контактный телефон:',
+        phone: '+380673926228',
+        header3: 'Мой профиль в Фейсбук:',
+        facebook: 'facebook.com/dmitry.scherbakov.585',
+        header4: 'Мой фидбек:',
+        feedback: 'Если нужно, могу сверстать такую страничку)'
+    };
 
-    var content = tmpl(html, {
-        data: contentArr
+    var myContent = tmpl(html, {
+        content: myInfo
     });
 
 
-    $('body').append(content);
+    $('body').append(myContent);
 
 });
