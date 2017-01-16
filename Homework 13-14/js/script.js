d24eee419a05ff0894c3f8ff0c84f628a2c6ed85
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-16 16:29:24 +0200

    hw7 0.2

--- Homework 13-14/js/script.js
+++ Homework 13-14/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+"use strict";
 
-  $( "#check" ).click(function() {
-    alert( "btn" );
-  });
 
+    $("#check").click(function() {
+
+        if ($("#three_first").is(':checked') && $("#two_second").is(':checked') && $("#one_third").is(':checked') && $("#three_third").is(':checked')) {
+            alert('верно!');
+            $("input:checkbox").prop("checked", false);
+
+        } else {
+            alert('не верно!');
+            $("input:checkbox").prop("checked", false);
+
+        }
+    });
 });
