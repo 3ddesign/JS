9f2ea8666a4f11d1b8d8b9477d83d80c8fed3829
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-20 13:20:56 +0200

    hw8 0.1

--- Homework 15-16/Task2/js/script.js
+++ Homework 15-16/Task2/js/script.js
@@ -1,5 +1,11 @@
-$(function() {
+    function Human(name, age, gender, hight, weight) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.hight = hight;
+        this.weight = weight;
+    }
 
-
-
-});
+var Worker.__proto__ = Human;
+Worker.my = 2;
+console.log(Human);
