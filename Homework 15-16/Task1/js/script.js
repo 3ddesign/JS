127c1645dfe3179fa6de872750a726ebeb3da741
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-23 16:06:41 +0200

    hw8 0.2

--- Homework 15-16/Task1/js/script.js
+++ Homework 15-16/Task1/js/script.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
+function GoogleCallback() {
+    console.log('arguments', arguments);
+}
+
+$(function() {
 
 
+
+    $.ajax({
+        url: 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=large&q=' + 'test1' + '&callback=GoogleCallback&context=?',
+        dataType: 'jsonp'
+
+    });
 
 });
+
+
+
+
+//
+// "The Google Web Search API is no longer available. Please migrate to the Google Custom Search API (https://developers.google.com/custom-search/)"
