6f6d864c6533eff57b406bf46ed55a04adf39272
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-24 12:56:30 +0200

    hw8 0.5

--- Homework 15-16/Task1/js/script.js
+++ Homework 15-16/Task1/js/script.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
 
-var request = $('.search');
-console.log(request);
-
-    $(function() {
-        var URL = "https://pixabay.com/api/?key=" + '4341489-c7135f07e924eb271481ce96f' + "&q=" + encodeURIComponent('kids');
+    $(".btn").bind('click mouseover', function() {
+        var request = $('.search').val();
+        console.log(request);
+        var URL = "https://pixabay.com/api/?key=" + '4341489-c7135f07e924eb271481ce96f' + "&q=" + encodeURIComponent(request);
         $.getJSON(URL, function(data) {
-            var num = 1;
-            if (parseInt(data.totalHits) > 2)
+            $('.searchresult').empty();
+            data.hits.splice(10);
+            if (parseInt(data.totalHits) > 0)
                 $.each(data.hits, function(i, hit) {
-
                     $('.searchresult').append('<a target="_blank" href="' + hit.pageURL + '">' + hit.pageURL + '</a>');
-                    num++;
-                    if (num == 12) {
-                        exit();
-                    }
                 });
             else
-                    $('.searchresult').append('<p>По Вашему запросу ничего не найдено...</p>');
+                $('.searchresult').append('<p>По Вашему запросу ничего не найдено...</p>');
         });
     });
 });
 
 
 
-// А гугл сказал: "The Google Web Search API is no longer available. Please migrate to the Google Custom Search API (https://developers.google.com/custom-search/)"
+//гугл сказал: "The Google Web Search API is no longer available. Please migrate to the Google Custom Search API (https://developers.google.com/custom-search/)"
