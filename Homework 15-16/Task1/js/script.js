3a0482c6db7f33994f46619a0f510e379856c35e
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-24 16:00:19 +0200

    hw8 1.0

--- Homework 15-16/Task1/js/script.js
+++ Homework 15-16/Task1/js/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
+    $('.search').keypress(function(e) {
+        if (e.which == 13) {
+            var request = $('.search').val();
+            var URL = "https://pixabay.com/api/?key=" + '4341489-c7135f07e924eb271481ce96f' + "&q=" + encodeURIComponent(request);
+            $.getJSON(URL, function(data) {
+                $('.searchresult').empty();
+                data.hits.splice(10);
+                if (parseInt(data.totalHits) > 0)
+                    $.each(data.hits, function(i, hit) {
+                        $('.searchresult').append('<a target="_blank" href="' + hit.pageURL + '">' + hit.pageURL + '</a>');
+                    });
+                else
+                    $('.searchresult').append('<p>По Вашему запросу ничего не найдено...</p>');
+            });
+        }
+    });
 
-    $(".btn").bind('click mouseover', function() {
+    $(".btn").on('click', function() {
         var request = $('.search').val();
-        console.log(request);
         var URL = "https://pixabay.com/api/?key=" + '4341489-c7135f07e924eb271481ce96f' + "&q=" + encodeURIComponent(request);
         $.getJSON(URL, function(data) {
             $('.searchresult').empty();
@@ -16,7 +31,5 @@ $(document).ready(function() {
         });
     });
 });
-
-
 
 //гугл сказал: "The Google Web Search API is no longer available. Please migrate to the Google Custom Search API (https://developers.google.com/custom-search/)"
