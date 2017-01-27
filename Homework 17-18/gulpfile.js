dd92ece3f26dfad4f2cebf5002ee791d5d290f3d
Dmitry Scherbakov <3ddesign@bk.ru>
2017-01-27 14:36:29 +0200

    hw9 1.1

--- Homework 17-18/gulpfile.js
+++ Homework 17-18/gulpfile.js
@@ -13,13 +13,6 @@ gulp.task('js', function() {
     return gulp.src('app/js/*.js')
         .pipe(uglify())
         .pipe(concat('common.js'))
-        .pipe(gulp.dest('dist/js/'));
-});
-
-gulp.task('watchjs', function() {
-    return gulp.src('app/js/*.js')
-        .pipe(uglify())
-        .pipe(concat('common.js'))
         .pipe(gulp.dest('app/js/'));
 });
 
@@ -40,16 +33,6 @@ gulp.task('sass', function() {
             "maxLineLen": 80,
             "uglyComments": true
         }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-gulp.task('csswatch', function() {
-    return gulp.src('app/sass/**/*.sass')
-        .pipe(sass())
-        .pipe(concat('main.css'))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({
             stream: true
@@ -73,7 +56,6 @@ gulp.task('clear', function() {
     return cache.clearAll();
 });
 
-
 gulp.task('img', function() {
     return gulp.src('app/img/**/*')
         .pipe(cache(imagemin({
@@ -87,18 +69,22 @@ gulp.task('img', function() {
         .pipe(gulp.dest('dist/img'));
 });
 
-
-
-gulp.task('watch', ['browser-sync', 'js', 'csswatch', 'watchjs'], function() {
+gulp.task('watch', ['browser-sync', 'js', 'sass'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/js/common.js', browserSync.reload);
 });
 
 gulp.task('build', ['clean', 'img', 'sass', 'libs', 'js'], function() {
 
     var buildFonts = gulp.src('app/fonts/**/*')
         .pipe(gulp.dest('dist/fonts'));
+
+    var buildJs = gulp.src('app/js/common.js')
+        .pipe(gulp.dest('dist/js'));
+
+    var buildCss = gulp.src('app/css/*.css')
+        .pipe(gulp.dest('dist/css'));
 
     var buildHtml = gulp.src('app/*.html')
         .pipe(gulp.dest('dist'));
