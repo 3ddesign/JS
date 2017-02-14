ef2a7e58d3faa3a68cf274d9b09ee614d9f9009f
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-14 16:24:35 +0200

    hw12 0.2

--- Homework 22-23/app/js/common.js
+++ Homework 22-23/app/js/common.js
@@ -1,3 +1,52 @@
-$(document).ready(function() {
+function Model() {
+    var self = this;
 
-});
+    self.data = ['test 1', 'test 2', 'test 3'];
+
+    self.addItem = function(item) {
+        if (item.length === 0) {
+            return;
+        }
+
+        self.data.push(item);
+
+        return self.data;
+    };
+
+    self.removeItem = function(item) {
+        var index = self.data.indexOf(item);
+
+        if (index === -1) {
+            return;
+        }
+
+        self.data.splice(index, 1);
+
+        return self.data;
+
+    };
+
+}
+
+function View(model) {
+    var self = this;
+
+    self.init = function() {
+        var wrapper = tmpl($('#wrapper-template').html());
+        $('body').html(wrapper);
+
+        
+    };
+
+    self.renderList = function(data) {
+
+    };
+
+    self.init();
+
+}
+
+
+function Controller() {
+
+}
