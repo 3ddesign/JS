30c4580ac03395786793a58a4e5ae2bb61546892
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-16 14:13:39 +0200

    hw12 0.5

--- Homework 23-24/app/js/common.js
+++ Homework 23-24/app/js/common.js
@@ -1,47 +1,39 @@
 function Model(data) {
-    var self = this;
-
+    let self = this;
     self.data = data;
 
-    // self.editItem = function(item) {
-    //     if (item.length === 0) {
-    //         return;
-    //     }
-    //
-    //     self.data.splice(index, 1, this.item);
-    //
-    //     return self.data;
-    // };
+    self.editItem = function(item, newitem) {
+        if (item.length === 0) {
+            return;
+        }
+        let index = self.data.indexOf(item);
+        self.data.splice(index, 1, newitem);
+        return self.data;
+    };
 
     self.addItem = function(item) {
         if (item.length === 0) {
             return;
         }
-
         self.data.push(item);
-
         return self.data;
     };
 
     self.removeItem = function(item) {
-        var index = self.data.indexOf(item);
-
+        let index = self.data.indexOf(item);
         if (index === -1) {
             return;
         }
-
         self.data.splice(index, 1);
-
         return self.data;
     };
 }
 
 function View(model) {
-    var self = this;
+    let self = this;
 
     function init() {
-        var wrapper = tmpl($('#wrapper-template').html());
-
+        const wrapper = tmpl($('#wrapper-template').html());
         $('body').append(wrapper);
         self.elements = {
             input: $('.item-value'),
@@ -52,7 +44,7 @@ function View(model) {
     }
 
     self.renderList = function(data) {
-        var list = tmpl($('#list-template').html(), {
+        const list = tmpl($('#list-template').html(), {
             data: data
         });
         self.elements.listContainer.html(list);
@@ -62,32 +54,39 @@ function View(model) {
 }
 
 function Controller(model, view) {
-    var self = this;
-
+    let self = this;
     view.elements.addBtn.on('click', addItem);
+    view.elements.listContainer.on('click', '.item-edit', editItem);
     view.elements.listContainer.on('click', '.item-delete', removeItem);
 
     function addItem() {
-        var newItem = view.elements.input.val();
-
+        let newItem = view.elements.input.val();
         model.addItem(newItem);
         view.renderList(model.data);
         view.elements.input.val('');
     }
 
-    function removeItem() {
-        var item = $(this).attr('data-value');
+    function editItem() {
+        let item = $(this).attr('data-value');
+        let editField = $(`<input type="text" class="item-editField" value="${item}">`);
+        let editBtn = $(`<img src="img/edit.svg">`);
+        $(this).parent().html('').append(editField).append(editBtn);
+        editBtn.on('click', function() {
+            model.editItem(item, editField.val());
+            view.renderList(model.data);
+        });
+    }
 
+    function removeItem() {
+        let item = $(this).attr('data-value');
         model.removeItem(item);
         view.renderList(model.data);
     }
-
 }
 
-
 $(function() {
-    var firstToDoList = ['test 1', 'test 2', 'test 3'];
-    var model = new Model(firstToDoList);
-    var view = new View(model);
-    var controller = new Controller(model, view);
+    let firstToDoList = ['Task 1', 'Task 2', 'Task 3'];
+    let model = new Model(firstToDoList);
+    let view = new View(model);
+    let controller = new Controller(model, view);
 });
